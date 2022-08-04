7e6f63fa33c1b38532a02bd114d72c7a47a8419c
pedroithiel <101153331+pedroithiel@users.noreply.github.com>
2022-08-04 20:29:41 -0300

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,71 @@
+var arrayNomeConvidado = [];
+
+function submit() {
+
+    var objInputNomeConvidado = document.getElementById("nomeConvidado");
+
+    var inputNomeConvidado = objInputNomeConvidado.value.trim();
+
+    if (inputNomeConvidado == "") {
+        
+        alert("Escreva o nome do convidado.");
+
+    }
+    else {
+
+
+        arrayNomeConvidado.push(inputNomeConvidado);
+
+        objInputNomeConvidado.value = "";
+
+        document.getElementById("mostrar").style.display = "inline-block";
+    }
+
+    //alert(inputNomeConvidado);
+    //alert(array);
+}
+
+function mostrar() {
+
+    document.getElementById("div_array").innerHTML = "";
+
+    for (var i = 0; i < arrayNomeConvidado.length; i++) {
+        document.getElementById("div_array").innerHTML += arrayNomeConvidado[i] + "<br>";
+    }
+
+    document.getElementById("organizar").style.display = "inline-block";
+
+}
+
+function organizar() {
+
+    arrayNomeConvidado.sort();
+    mostrar();
+
+}
+
+function pesquisar() {
+    var found = 0;
+
+    //for (Valor Inicial; Condicao; Aumentar o valor);
+    var inputPesquisarNomeConvidado = document.getElementById("pesquisa").value.trim();
+
+    if (inputPesquisarNomeConvidado == "") {
+
+        alert("Escreva algo para pesquisar");
+
+    }
+    else {
+
+        for (var i = 0; i < arrayNomeConvidado.length; i++) {
+
+            if (inputPesquisarNomeConvidado == arrayNomeConvidado[i]) {
+                found++;
+            }
+        }
+
+        document.getElementById("text_nome_encontrado").innerHTML = "Nome econtrado " + found + " vezes.";
+    }
+}
+
+
